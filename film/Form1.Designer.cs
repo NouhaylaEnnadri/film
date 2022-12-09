d89b666d1237afcd51111507ae286d4fd078fdf3
Nouhayla <nouhayla.ennadri@usmba.ac.ma>
2022-12-09 13:25:45 +0100

    //

--- film/Form1.Designer.cs
+++ film/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.log_in_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOG IN";
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(58, 204);
+            this.email.Name = "email";
+            this.email.PlaceholderText = "Email";
+            this.email.Size = new System.Drawing.Size(305, 31);
+            this.email.TabIndex = 1;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(58, 264);
+            this.password.Name = "password";
+            this.password.PlaceholderText = "password";
+            this.password.Size = new System.Drawing.Size(305, 31);
+            this.password.TabIndex = 2;
+            // 
+            // log_in_btn
+            // 
+            this.log_in_btn.Location = new System.Drawing.Point(146, 342);
+            this.log_in_btn.Name = "log_in_btn";
+            this.log_in_btn.Size = new System.Drawing.Size(112, 34);
+            this.log_in_btn.TabIndex = 3;
+            this.log_in_btn.Text = "LOG IN";
+            this.log_in_btn.UseVisualStyleBackColor = true;
+            this.log_in_btn.Click += new System.EventHandler(this.log_in_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(421, 429);
+            this.Controls.Add(this.log_in_btn);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox email;
+        private TextBox password;
+        private Button log_in_btn;
     }
 }
--- film/Form1.cs
+++ film/Form1.cs
@@ -1,10 +1,49 @@
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
 namespace film
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
+        #region connexion 
+        static string Chaine = @"Data Source = DESKTOP - H5O83VN\SQLEXPRESS;Initial Catalog = FILM; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+";
+
+        static SqlConnection cnx = new SqlConnection(Chaine);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        #endregion
+
+        private void log_in_btn_Click(object sender, EventArgs e)
+        {
+
+            cnx.Open();
+
+           
+
+
+                SqlCommand cmd = new SqlCommand("check_login", cnx);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@email", SqlDbType.NVarChar).Value = email.Text.ToString();
+                cmd.Parameters.Add("@password", SqlDbType.NVarChar).Value = password.Text.ToString();
+                SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Login succeful");
+            }
+            else
+            {
+                MessageBox.Show("error");
+            }
+
+            cnx.Close();
+        }
+
     }
 }
