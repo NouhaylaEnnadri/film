8d671ca13a608bc3323010efe3ea403d598184a5
Nouhayla <nouhayla.ennadri@usmba.ac.ma>
2022-12-09 16:00:26 +0100

    //

--- film/Form1.cs
+++ film/Form1.cs
@@ -12,34 +12,36 @@ namespace film
             InitializeComponent();
         }
         #region connexion 
-        static string Chaine = @"Data Source = DESKTOP - H5O83VN\SQLEXPRESS;Initial Catalog = FILM; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
-";
+        static string Chaine = @"Data Source=DESKTOP-H5O83VN\SQLEXPRESS;Initial Catalog=FILM;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; 
+
 
         static SqlConnection cnx = new SqlConnection(Chaine);
         static SqlCommand cmd = new SqlCommand();
         static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
         #endregion
 
         private void log_in_btn_Click(object sender, EventArgs e)
         {
 
+           
+            
             cnx.Open();
 
-           
-
-
-                SqlCommand cmd = new SqlCommand("check_login", cnx);
-                cmd.CommandType = CommandType.StoredProcedure;
+            SqlCommand cmd = new SqlCommand("login", cnx);
+            cmd.Connection = cnx;
+            cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.Add("@email", SqlDbType.NVarChar).Value = email.Text.ToString();
                 cmd.Parameters.Add("@password", SqlDbType.NVarChar).Value = password.Text.ToString();
                 SqlDataReader dr = cmd.ExecuteReader();
+
             if (dr.Read())
             {
-                MessageBox.Show("Login succeful");
+                MessageBox.Show("Login sucessful");
             }
             else
             {
-                MessageBox.Show("error");
+                MessageBox.Show("login failed");
             }
 
             cnx.Close();
---
+++ film/movies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace film
+{
+    public partial class movies : Form
+    {
+        public movies()
+        {
+            InitializeComponent();
+        }
+
+        static string Chaine = @"Data Source=DESKTOP-H5O83VN\SQLEXPRESS;Initial Catalog=FILM;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        static SqlConnection cnx = new SqlConnection(Chaine);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+
+    }
+}
